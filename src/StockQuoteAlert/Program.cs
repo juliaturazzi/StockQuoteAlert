5d32b6a7e4be2f86aad7fd62113a99288880ae17
juliaturazzi <juliaturazzi@gmail.com>
2025-11-23 17:49:39 -0300

    chore: add initial program structure and logging

--- src/StockQuoteAlert/Program.cs
+++ src/StockQuoteAlert/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using StockQuoteAlert.Domain.Entities;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+try
+{
+    Log.Information("Starting up Stock Quote Alert...");
+
+    if (args.Length < 3)
+    {
+        Log.Error("Incorrect number of arguments. Usage: StockQuoteAlert <ASSET_TICKER> <SELLING_PRICE> <BUYING_PRICE>");
+        return;
+    }
+
+    var assetTicker = args[0];
+    var sellingPrice = decimal.Parse(args[1]);
+    var buyingPrice = decimal.Parse(args[2]);
+
+    Log.Information("Monitoring asset {AssetTicker} for selling price {SellingPrice} and buying price {BuyingPrice}",
+        assetTicker, sellingPrice, buyingPrice);
+
+    var builder = Host.CreateApplicationBuilder(args);
+
+    builder.Services.AddSerilog((services, lc) => lc
+        .ReadFrom.Configuration(builder.Configuration));
+
+    builder.Services.Configure<NotificationSettings>(
+        builder.Configuration.GetSection("NotificationSettings"));
+
+    // builder.Services.AddSingleton<IEmailService, SmtpEmailService>();
+    // builder.Services.AddHttpClient<IStockPriceService, HgFinanceService>();
+
+    // builder.Services.AddHostedService<StockMonitorWorker>();
+
+    var host = builder.Build();
+
+    await host.RunAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application start-up failed");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
